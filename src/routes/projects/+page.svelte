0d174bbb9405260819d1486c1fac781ba4992edb
Shiheb Din شهاب الدين <d3j1x@yahoo.com>
2023-02-18 12:09:11 +0000

    projects animation

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -6,10 +6,51 @@
 <div class="text-column">
 	<h1 style="background-color: black;margin:0 20px;border:solid red 0.2rem;opacity:0.9">Opensource Projects</h1>
 
-	<div class="secret">
+	<!-- <div class="secret">
 		<h2>CYBER-SECRET</h2>
 		<p>
-		This is a <a href="https://github.com/d3j1x/cyber-secret">Cyber-secret</a> repo. You can Explore it by following the <a href="https://d3j1x.github.io/cyber-secret/">link</a>.
+		This is a <a href="https://github.com/d3j1x/cyber-secret">Cyber-Secret</a> repo. You can Explore it by following the <a href="https://d3j1x.github.io/cyber-secret/">link</a>.
+		</p>
+	</div> -->
+
+	<div id="loading">
+		<div class="waviy">
+			<br>
+ <span style="--i:1">C</span>
+ <span style="--i:2">Y</span>
+ <span style="--i:3">B</span>
+ <span style="--i:4">E</span>
+ <span style="--i:5">R</span>
+ <span style="--i:6">-</span>
+ <span style="--i:7">S</span>
+ <span style="--i:8">E</span>
+ <span style="--i:9">C</span>
+ <span style="--i:10">R</span>
+ <span style="--i:11">E</span>
+ <span style="--i:12">T</span>
+	</div>
+		<p>
+			This is a <a href="https://github.com/d3j1x/cyber-secret">Cyber-Secret</a> repo. You can Explore it by following the <a href="https://d3j1x.github.io/cyber-secret/">link</a>.
+		</p>
+	</div>
+
+
+
+	<div id="loading">
+		<div class="waviy">
+			<br>
+ <span style="--i:1">S</span>
+ <span style="--i:2">M</span>
+ <span style="--i:3">R</span>
+ <span style="--i:4">T</span>
+ <span style="--i:5">-</span>
+ <span style="--i:6">M</span>
+ <span style="--i:7">R</span>
+ <span style="--i:8">K</span>
+ <span style="--i:9">T</span>
+	</div>
+		<p>
+			This is a <a href="https://github.com/smrket/Smrt-Mrkt">Smrt-Mrkt</a> repo. You can Explore it by following the <a href="https://smrket.github.io/Smrt-Mrkt/">link</a>.
 		</p>
 	</div>
 
@@ -42,7 +83,7 @@
 		border-radius: 20%;
 	}
 
-	.secret{
+	/* .secret{
 		background-color: black;
 		margin: 10px auto;
 		text-align: center;
@@ -54,7 +95,35 @@
 		font-weight: bolder;
 		color: #3DFF33;
 		
+	} */
+
+	#loading {
+	width: 80vh;
+  	background-color: black;
+  	margin: 10px auto;
+	text-align: center;
+	border: dotted yellow;
 	}
+
+.waviy {
+  position: relative;
+ 
+}
+.waviy span {
+  	position: relative;
+  	display: inline-block;
+  	font-size:larger;
+	font-weight: bolder;
+	color: #3DFF33;
+ 	text-transform: uppercase;
+  	animation: flip 3s infinite;
+  	animation-delay: calc(.2s * var(--i))
+}
+@keyframes flip {
+  0%,80% {
+    transform: rotateY(360deg) 
+  }
+}
 
 	
 
